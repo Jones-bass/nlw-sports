52172408e35571295387b3ec85edbbb50a9836fa
jones-bass <jonesbass.tb@gmail.com>
2023-12-16 06:03:51 -0300

    Component Card Mobile

--- nlw-sports/mobile/src/components/Background/styles.ts
+++ nlw-sports/mobile/src/components/Background/styles.ts
@@ -5,5 +5,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: THEME.COLORS.BACKGROUND_800,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
 });
--- nlw-sports/mobile/src/screens/Home/index.tsx
+++ nlw-sports/mobile/src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 
-import { Image } from 'react-native';
+import { FlatList, Image } from 'react-native';
 
 import { Background } from "../../components/Background";
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { styles } from './styles';
 import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
@@ -19,6 +21,18 @@ export function Home() {
         title="Encontre seu duo!"
         subtitle="Selecione o game que deseja jogar..."
       />
+        <FlatList
+          data={GAMES}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
 
     </Background>
   );
